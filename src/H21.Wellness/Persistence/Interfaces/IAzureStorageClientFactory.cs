dd0299befaacc5eb05545174d47514ce6f5a141d
Sean Sanchez <atsanche@microsoft.com>
2021-10-12 11:56:49 -0700

    Add BaseAzureBlobStorageRepository

--- src/H21.Wellness/Persistence/Interfaces/IAzureStorageClientFactory.cs
+++ src/H21.Wellness/Persistence/Interfaces/IAzureStorageClientFactory.cs
@@ -1,13 +1,23 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Microsoft.Azure.Cosmos.Table;
 
-namespace H21.Wellness.Persistence.Interfaces
+namespace H21.Wellness.Persistence
 {
     public interface IAzureStorageClientFactory
     {
         Task<CloudTable> GetCloudTableAsync(
             string tableName,
+            CancellationToken cancellationToken = default);
+
+        Task<BlobContainerClient> GetBlobContainerClientAsync(
+            string blobContainerName,
+            CancellationToken cancellationToken = default);
+
+        Task<BlobClient> GetBlobClientAsync(
+            string blobContainerName,
+            string blobName,
             CancellationToken cancellationToken = default);
     }
 }
