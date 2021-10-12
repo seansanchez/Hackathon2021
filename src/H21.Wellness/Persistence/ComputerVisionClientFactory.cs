630f03f7db0c9dc87c0cf77edac4288a612b94bd
anjanaChromy <ANACKROY@MICROSOFT.COM>
2021-10-12 12:23:40 -0700

    added client code

--- src/H21.Wellness/Persistence/ComputerVisionClientFactory.cs
+++ src/H21.Wellness/Persistence/ComputerVisionClientFactory.cs
@@ -1,15 +1,45 @@
-﻿using H21.Wellness.Persistence.Interfaces;
+﻿using H21.Wellness.Extensions;
+using H21.Wellness.Persistence.Interfaces;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Extensions.Options;
 using System.Threading.Tasks;
 
 namespace H21.Wellness.Persistence
 {
     public class ComputerVisionClientFactory : IComputerVisionClientFactory
-    {   /// <summary>
+    {
+        private readonly IOptions<ComputerVisionOptions> _options;
+        public ComputerVisionClientFactory(IOptions<ComputerVisionOptions> options)
+        {
+            options.ThrowIfNull(nameof(options));
+            options.Value.ThrowIfNull($"{nameof(options)}.{nameof(options.Value)}");
+            options.Value.ApiKey.ThrowIfNullOrWhitespace($"{nameof(options)}.{nameof(options.Value)}.{nameof(options.Value.ApiKey)}");
+            options.Value.Endpoint.ThrowIfNullOrWhitespace($"{nameof(options)}.{nameof(options.Value)}.{nameof(options.Value.Endpoint)}");
+
+
+            _options = options;
+        }
+
+
+        protected ApiKeyServiceClientCredentials Credentials
+        {
+            get
+            {
+                return new ApiKeyServiceClientCredentials(_options.Value.ApiKey);
+            }
+        }
+
+        protected string Endpoint
+        {
+            get
+            {
+                return _options.Value.Endpoint;
+            }
+        }
+
+
+        /// <summary>
         /// Sends a URL to Cognitive Services and generates tags for it.
         /// </summary>
         /// <param name="imageUrl">The URL of the image for which to generate tags.</param>
@@ -29,10 +59,7 @@ namespace H21.Wellness.Persistence
                 TagResult analysisResult = await client.TagImageAsync(imageUrl, "EN");
                 return analysisResult;
             }
-
-            // -----------------------------------------------------------------------
-            // KEY SAMPLE CODE ENDS HERE
-            // -----------------------------------------------------------------------
+------------------------------------------------------------------
         }
     }
 }
--- src/H21.Wellness/Persistence/Interfaces/IComputerVisionClientFactory.cs
+++ src/H21.Wellness/Persistence/Interfaces/IComputerVisionClientFactory.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace H21.Wellness.Persistence.Interfaces
 {
     public interface IComputerVisionClientFactory
     {
+        Task<TagResult> GenerateTagsForUrlAsync(string imageUrl);
     }
 }
