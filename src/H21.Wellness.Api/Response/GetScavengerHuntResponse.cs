fa7dd189f7bfe8c7adab1fcb8d555bb944f94482
Sean Sanchez <atilano.e.sanchez@outlook.com>
2021-10-12 13:27:17 -0700

    Add API Stubs (#11)
    * add stubs
    
    * update appsettings
    
    * add from route attribute

---
+++ src/H21.Wellness.Api/Response/GetScavengerHuntResponse.cs
@@ -0,0 +1,9 @@
+﻿using H21.Wellness.Models;
+
+namespace H21.Wellness.Api.Response
+{
+    public class GetScavengerHuntResponse
+    {
+        public ScavengerHuntModel ScavengerHunt { get; set; }
+    }
+}
