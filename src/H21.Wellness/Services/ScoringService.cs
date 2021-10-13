bb96d529c0f4e5d7cc657f694a2b6393fe50a2dd
Steven Countryman II <stevenco@microsoft.com>
2021-10-13 16:36:33 -0700

    Improved scoring logic

--- src/H21.Wellness/Services/ScoringService.cs
+++ src/H21.Wellness/Services/ScoringService.cs
@@ -12,9 +12,9 @@ namespace H21.Wellness.Services
 
            var totalItems = 10;
            var totalTime = 20;
-           var maxItemCountScore = 100;
 
-           var baseScore = (uint)Math.Round(Math.Log(numItemsCompleted / 160.0) / Math.Log(maxItemCountScore / 160.0));
+           var baseScoreRatio = Math.Log((numItemsCompleted / 2.0) + 1.0) / Math.Log((totalItems / 2.0) + 1.0);
+           var baseScore = (uint) Math.Round(baseScoreRatio * 100.0);
 
            var timeBonus = (totalTime - completedInMinutes) / (totalTime / totalItems);
            var bonusScore = (uint)Math.Floor((decimal)timeBonus) * 5;
